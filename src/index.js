7ab875115c3dda89828fd9847ed4d537f04b7d7d
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-02-24 10:00:57 +0300

    starting files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("React is loading...");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
